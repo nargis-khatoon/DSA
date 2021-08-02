27fc3a4aaf319c564b67afeb981f82e64a5b464f
Nargis <ahmadnargis95@gmail.com>
2021-08-02 13:28:35 +0530

    added new code

---
+++ GRAPHS/detectCycle.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // 0 based indexed graph code
+
+
+class Solution {
+
+public:
+    bool dfsCycle(int node, int par, vector<int> &vis, vector<int> adj[]) {
+        vis[node] = 1; 
+        for(auto it: adj[node]) {
+            if(!vis[it]) {
+                if(checkForCycle(it, node, vis, adj)) 
+                    return true; 
+            }
+            else if(it!=par) 
+                return true; 
+        }
+        
+        return false; 
+    }
+public:
+	bool isCycle(int V, vector<int>adj[]){
+	    vector<int> vis(V+1, 0); 
+	    for(int i = 0;i<V;i++) {
+	        if(!vis[i]) {
+	            if(checkForCycle(i, -1, vis, adj)) return true; 
+	        }
+	    }
+	    
+	    return false; 
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isCycle(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}  // } Driver Code Ends
